22a8be3c877f4cab4a0293213ef04d31c44a6662
Muhamed Usama <muhamedusama92@gmail.com>
2025-01-20 23:21:01 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Smooth Scroll for Navigation Links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
